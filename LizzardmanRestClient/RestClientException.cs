8631e9476ab9b113a4834bd3881bf715b796c8e7
Artem Zelinskiy <zelinsky@outsoft.com>
2016-09-23 14:47:54 +0300

    first commit

---
+++ LizzardmanRestClient/RestClientException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Net;
+
+namespace LizzardmanRestClient
+{
+    public class RestClientException : Exception
+    {
+        public HttpStatusCode StatusCode { get; set; }
+        public bool IsNetwork { get; set; }
+        new public Exception InnerException { get; set; }
+        new public string Message { get; set; }
+    }
+}
